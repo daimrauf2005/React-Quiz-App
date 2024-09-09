80ddb3f2c16fa5446cdde2ba6107ac33cad92e8c
daimrauf2005 <153510075+daimrauf2005@users.noreply.github.com>
2024-09-09 02:59:18 -0800

    Quiz App Create

---
+++ src/App.jsx
@@ -0,0 +1,144 @@
+import axios from "axios";
+import { useRef, useState } from "react";
+import { useEffect } from "react";
+
+function App(){
+  const [render,setRender] = useState([])
+  const [questionState,setQuestionState] = useState(0)
+  const [score,setScore] = useState(0)
+  const [WrongScore,setWrongScore] = useState(0)
+  const [quizComplete, setQuizComplete] = useState(false)
+  const checkedInput = useRef([])
+
+ //Api Handling
+  try {
+    useEffect(()=>{
+      axios('https://the-trivia-api.com/v2/questions')
+      .then((res)=>{
+        console.log(res.data)
+        setRender(shuffleArray(res.data))
+      })
+      .catch((err)=>{
+        console.log(err);
+        
+      })
+    },[])
+  } catch (error) {
+    console.log(error);
+  }
+
+  // shuffle Array 
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    
+    return array;
+  }
+
+//  Next Question 
+  function NextQuetsion(){
+    
+    console.log(render.length);
+    
+    const checkedButton = checkedInput.current.find(input => input.checked);
+
+    if(!checkedButton){
+      alert('Please select an option');
+      return; 
+    }
+
+    
+      const selectedValue = checkedButton.value;
+      console.log("Selected answer:", selectedValue);
+
+      // store selectAnswer
+      if(selectedValue === render[questionState].correctAnswer){
+        setScore(score + 1)
+        console.log(score);
+      }else{
+        setWrongScore(WrongScore + 1)
+        console.log(WrongScore);
+      }
+      
+      
+  
+    const next = questionState + 1
+    if(next >= render.length - 1){
+      setQuizComplete(true);
+      alert(`Complete Quiz. Your score is ${score}/9`)
+      // setQuestionState(0)
+      // setScore(0);
+      // setWrongScore(0);
+      setRender(shuffleArray(render))
+    }else{
+      setQuestionState(next);
+    }
+
+
+
+    checkedInput.current.forEach(input=>{
+      if(input){
+        input.checked = false;
+      }
+    })
+  }
+
+  
+  
+  //return JSX
+  return (
+    <>
+    {render.length > 0 ? (
+        <div>
+          {!quizComplete ? (
+            <>
+              <div className="container">
+              <h1 className="quiz">Quiz App</h1>
+              <h2>Q{questionState + 1} : {render[questionState].question.text}</h2>
+              <hr />
+              <div className="main">
+              <ol>
+                {shuffleArray([...render[questionState].incorrectAnswers, render[questionState].correctAnswer])
+                  .map((item, index) => (
+                      <li key={index} className="li">
+                        <input 
+                          id={item}
+                          name="quiz"
+                          type="radio"
+                          value={item}
+                          ref={e => checkedInput.current[index] = e}
+                        />
+                        <label className="li-label" htmlFor={item}>{item}</label>
+                      </li>
+                  ))}
+              </ol>
+              </div>
+              <button className="btn" onClick={NextQuetsion}>Next</button>
+              </div>
+            </>
+          ) : (
+            <div className="quiz-complete">
+              <h1>Quiz Complete</h1>
+              <h3>{`Final Score: ${score}/9`}</h3>
+              <div>
+                <h3>{`Correct Answers: ${score}`}</h3>
+                <h3>{`Wrong Answers: ${WrongScore}`}</h3>
+                <h1>Total: 9</h1>
+                <button className="btn-2" onClick={() => window.location.reload()}>Again</button>
+              </div>
+            </div>
+          )}
+        </div>
+      ) : (
+         <>
+                 <h1 className="loading">Loading...</h1>
+         </>
+      )}
+    </>
+  );
+}
+
+export default App;
